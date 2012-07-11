486744db09851ab9ca722b92a3abe3dc26e57f9c
Georg Schönberger <gschoenberger@thomas-krenn.com>
2012-07-11 08:10:14 +0200

    Added getting device size via blockdev command

--- src/perfTest/DeviceTest.py
+++ src/perfTest/DeviceTest.py
@@ -4,11 +4,13 @@ Created on 04.07.2012
 @author: gschoenb
 '''
 
+import logging
+import subprocess
+
 class DeviceTest(object):
     '''
     A generic class for a performance test.
     '''
-
 
     def __init__(self,testname,filename):
         '''
@@ -19,6 +21,7 @@ class DeviceTest(object):
         
         ## The output file for the fio job test results.
         self.__testname = testname
+        
         ## The data file for the test, can be a whole device also.
         self.__filename = filename
         
@@ -29,3 +32,30 @@ class DeviceTest(object):
     def getFilename(self):
         ''' Return the name of the data file or device. '''
         return self.__filename
+    
+    def getDevSizeKB(self):
+        '''
+        Get the device size in KByte.
+        The function calls 'blockdev' to determine device sector size
+        and sector count.
+        @return [True,size] on success, [False,0] if an error occured.
+        '''
+        out = subprocess.Popen(['blockdev','--getss',self.__filename],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+        (stdout,stderr) = out.communicate()
+        if stderr != '':
+            logging.error("blockdev --getss encountered an error: " + stderr)
+            return [False,0]
+        else:
+            sectorSize = int(stdout)
+            out = subprocess.Popen(['blockdev','--getsz',self.__filename],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+            (stdout,stderr) = out.communicate()
+            if stderr != '':
+                logging.error("blockdev --getsz encountered an error: " + stderr)
+                return [False,0]
+            else:
+                sectorCount = long(stdout)
+                devSzKB = (sectorCount * sectorSize) / 1024
+                logging.info("#Device" + self.__filename + " sector count: " + str(sectorCount))
+                logging.info("#Device" + self.__filename + " sector size: " + str(sectorSize))
+                logging.info("#Device" + self.__filename + " size in KB: " + str(devSzKB))
+                return [True,devSzKB]
