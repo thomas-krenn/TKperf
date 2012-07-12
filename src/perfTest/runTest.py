45c0d6b0ab5232e573710d3dbada37c49d9accc4
Georg Schönberger <gschoenberger@thomas-krenn.com>
2012-07-12 15:41:05 +0200

    Exit if device is mounted and print warning that any data will be lost on given device
    Only continue if 'y' is pressed

--- src/perfTest/runTest.py
+++ src/perfTest/runTest.py
@@ -6,6 +6,8 @@ Created on 27.06.2012
 
 
 import argparse
+import logging
+
 from fio.FioJob import FioJob
 from perfTest.SsdTest import SsdTest
 
@@ -21,14 +23,38 @@ if __name__ == '__main__':
     parser.add_argument("filename",help="data file or device name to run fio test on")
     
     parser.add_argument("-v","--version", help="get the version information", action='version',version=fioVersion)
+    parser.add_argument("-d","--debug", help="get detailed debug information",action ='store_true')
+    parser.add_argument("-q","--quiet", help="turn off logging of info messages",action ='store_true')
     
     args = parser.parse_args()
+    if args.debug == True:
+        logging.basicConfig(filename=args.testname+'.log',level=logging.DEBUG)
+    if args.quiet == True:
+        logging.basicConfig(filename=args.testname+'.log',level=logging.WARNING)
+    else:
+        logging.basicConfig(filename=args.testname+'.log',level=logging.INFO)
+    
     if args.mode == "hdd":
         print "hdd on"
     if args.mode == "ssd":
-        print "ssd on"
         myTest = SsdTest(args.testname,args.filename)
-        myTest.wlIndPrec()
-        myTest.IOPSTestReport()
+        if myTest.checkDevIsMounted() == True:
+            print "!!!WARNING!!!"
+            print "You are writing to a mounted device, this is highly dangerous!"
+            exit(0)
+        if myTest.checkDevIsAvbl() == True:
+            print "!!!Attention!!!"
+            print "All data on " + args.filename + " will be lost!"
+            print "Are you sure you want to continue? (In case you really know what you are doing.)"
+            print "Press 'y' to continue, any key to stop:"
+            key = raw_input()
+            if key != 'y':
+                exit(0)
+        else:
+            print "You are not using a valid device or partition!"
+            exit(1)        
+        
+        #myTest.runWriteSatTest()
+        myTest.runTpTest()
         print myTest.getTestname()
         print myTest.getFilename()
