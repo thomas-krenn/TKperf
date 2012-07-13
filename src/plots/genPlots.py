2671dc093e4c5e75231c88f121d7c3d67d929252
Georg Schönberger <gschoenberger@thomas-krenn.com>
2012-07-13 11:25:04 +0200

    Throughput plots and new average computation

--- src/plots/genPlots.py
+++ src/plots/genPlots.py
@@ -80,7 +80,6 @@ def stdyStConvPlt(toPlot):
     
     plt.clf()#clear
     #fetch number of rounds, we want to include all rounds
-    #as stdy state was reached at rnds, it must be included
     x = range(rnds + 1)
     for i in range(len(lines)):
         plt.plot(x,lines[i],'o-',label='bs='+pT.SsdTest.SsdTest.bsLabels[i])
@@ -98,15 +97,16 @@ def mes2DPlt(toPlot):
     The plot includes:
     -Lines of the workloads
     -Each line consists of the average of IOPS per round
-    for each block size.
+    for each block size in the measurement window!
     Therefore the x axes are the block sizes, the plotted lines
     are the different workloads (from 100% read to 100% write). The
-    y axes is the average of IOPS over all rounds for each block size
+    y axes is the average of IOPS over the measurement window for each block sizes
     and workload.
     The figure is saved as SsdTest.Testname-mes2DPlt.png.
     @param toPlot A SsdTest object.
     '''
     mixWLds = []
+    mesWin = toPlot.getStdyRnds() #get measurement window, only include these values
     #each row will be a workload percentage
     for i in range(len(pT.SsdTest.SsdTest.mixWlds)):
         mixWLds.append([])
@@ -114,15 +114,19 @@ def mes2DPlt(toPlot):
         for bs in range(len(pT.SsdTest.SsdTest.bsLabels)):
             mixWLds[i].append(0)
     matrices = toPlot.getRndMatrices()    
-    for rndMat in matrices:
+    
+    #limit the matrices to the measurement window
+    for j in mesWin:
+        rndMat = matrices[j]
         #each row is a percentage of a workload
         for i,row in enumerate(rndMat):
             #in each row are the different block sizes
             for bs in range(len(row)):
-                #calculate average
+                #calculate average iteratively
                 if mixWLds[i][bs] != 0:
+                    mixWLds[i][bs] *= bs
                     mixWLds[i][bs] += row[bs]
-                    mixWLds[i][bs] = (mixWLds[i][bs]) / 2
+                    mixWLds[i][bs] = (mixWLds[i][bs]) / (bs+1)
                 else:
                     mixWLds[i][bs] = row[bs]
     plt.clf()#clear plot
@@ -135,7 +139,7 @@ def mes2DPlt(toPlot):
     #TODO Scale axes log   
          
     plt.xticks(x)
-    plt.title("IOPS test")
+    plt.title("IOPS Measurement Plot")
     plt.xlabel("Block Size (KB)")
     plt.ylabel("IOPS")
     plt.legend()
@@ -181,10 +185,94 @@ def writeSatLatPlt(toPlot):
     plt.legend()
     plt.savefig(toPlot.getTestname()+'-writeSatLatPlt.png',dpi=300)
     
+def tpStdyStConvPlt(toPlot,mode):
+    '''
+    Generate a steady state convergence plot for throughput measurements.
+    The plot consists of:
+    -Measured bandwidths per round per block size
+    -All lines are the different block sizes
+    -x axes is the number of all rounds
+    -y axes is the bw of the corresponding round
+    The figure is saved as SsdTest.Testname-bw-stdyStConvPlt.png.
+    @param toPlot A SsdTest object.
+    @param mode String "read" or "write"
+    '''
+    matrices = toPlot.getTPRndMatrices()
+    rnds = len(matrices[0][0])#fetch the number of total rounds
+    bsLens = len(matrices)#fetch the number of bs, each row is a bs in the matrix
     
+    #initialize matrix for plotting
+    lines = []
+    for i in range(bsLens):
+        lines.append([])
     
+    plt.clf()#clear
+    x = range(rnds)#determined by len of matrix
+    for i,rndMat in enumerate(matrices):
+        if mode == "read":
+            row = rndMat[0]#plot to the read row
+        else:
+            row = rndMat[1]#plot the write row
+        plt.plot(x,row,'o-',label='bs='+pT.SsdTest.SsdTest.tpBsLabels[i])
     
+    plt.xticks(x)
+    plt.title("TP "+mode+" Steady State Convergence Plot")
+    plt.xlabel("Round")
+    plt.ylabel("BW KB/s")
+    plt.legend()
+    plt.savefig(toPlot.getTestname()+'-bw-'+mode+'-stdyStConvPlt.png',dpi=300)
     
+def tpMes2DPlt(toPlot):
+    '''
+    Generate a measurement 2D plot for throughput measurements.
+    The plot includes:
+    -One line for read, one line for write
+    -Each line consists of the average of IOPS per round
+    for each block size in the measurement window!
+    Therefore the x axes are the block sizes, the y axes is
+    the average over the measurement window for each block size.
+    The figure is saved as SsdTest.Testname-bw-mes2DPlt.png.
+    @param toPlot A SsdTest object.
+    '''
+    wlds = [] #read and write workload mode
+    
+    #one row for read, one for write
+    for i in range(2):
+        wlds.append([])
+        #in each row will be the different block sizes
+        for bs in range(len(pT.SsdTest.SsdTest.tpBsLabels)):
+            wlds[i].append(0)
+    matrices = toPlot.getTPRndMatrices()    
+    
+    #each row of the matrix is a block size
+    for j,bs in enumerate(matrices):
+        #each block size has read and write
+        for i,row in enumerate(bs):
+            #read and write have their round values
+            for rnd in toPlot.getStdyRnds():
+                #calculate average iteratively
+                if wlds[j][i] != 0:
+                    wlds[j][i] *= rnd
+                    wlds[j][i] += row[rnd]
+                    wlds[j][i] = (wlds[j][i]) / (rnd+1)
+                else:
+                    wlds[j][i] = row[rnd]
+    print wlds
+    plt.clf()#clear
+    x = [1024,64]#FIXME Add correct block sizes here
+    for i in range(len(wlds)):
+        if i == 0:
+            label = "read"
+        else:
+            label = "write"
+        plt.plot(x,wlds[i],'o-',label=label)
+    
+    plt.xticks(x)
+    plt.title("TP Measurement Plot")
+    plt.xlabel("Block Size (KB)")
+    plt.ylabel("BW KB/s")
+    plt.legend()
+    plt.savefig(toPlot.getTestname()+'-bw-mes2DPlt.png',dpi=300)
     
     
     
