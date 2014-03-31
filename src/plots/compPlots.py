2c067c30c71143faf9f51a947878db46fc35a786
Georg Schönberger <gschoenberger@thomas-krenn.com>
2014-03-31 07:34:21 +0200

    Basic throughput compare plot

--- src/plots/compPlots.py
+++ src/plots/compPlots.py
@@ -60,14 +60,11 @@ def compIOPSPlt(testsToPlot):
     width = 1/len(testsToPlot)
     for i in range(len(x)):
         x[i] = x[i] + (i * width)
-    print width
     for i,tests in enumerate(testsToPlot):
         test = tests.getTests()['iops']
         pgp.calcMsmtTable(test, 'IOPS')
         mixWLds = test.getTables()[0]
         testIOPS = [mixWLds[0][6],mixWLds[3][6],mixWLds[6][6]]
-        print testIOPS
-        print x
         plt.bar(x, testIOPS, width,label=test.getTestname(),color = __colorTable__[i])
         x = [v + width for v in x]
     ticksx = [(len(testsToPlot)/2) * width, 1 + width + 0.5, 2 + (2 * width) + 0.5 ]
@@ -79,3 +76,36 @@ def compIOPSPlt(testsToPlot):
     plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.07),
                ncol=3,fancybox=True, shadow=True,prop={'size':10})
     plt.savefig('compIOPSPlt.png',dpi=300)
+
+def compTPPlt(testsToPlot):
+    """
+    Compare multiple tests and create a throughput plot.
+    All test objects in testsToPlot are plotted.
+    
+    Keyword arguments:
+    testsToPlot -- an array of perfTest objects
+    """
+    plt.clf()#clear plot
+    y = range(3)
+    height = 1/len(testsToPlot)
+    for i in range(len(y)):
+        y[i] = y[i] + (i * height)
+    for i,tests in enumerate(testsToPlot):
+        test = tests.getTests()['tp']
+        pgp.calcMsmtTPTable(test)
+        wlds = test.getTables()[0]
+        testRTP = [wlds[0][2],wlds[0][1],wlds[0][0]]
+        print testRTP
+        print y
+        plt.barh(y, testRTP, height, label=test.getTestname(),color = __colorTable__[i])
+        y = [v + height for v in y]
+    ticksy = [(len(testsToPlot)/2) * height, 1 + height + 0.5, 2 + (2 * height) + 0.5 ]
+    labelsy = ['8k','64k','1024k']
+    plt.yticks(ticksy, labelsy)
+    plt.grid()
+    plt.suptitle("TP Measurement Test",fontweight='bold')
+    plt.xlabel("Bandwidth (MB/s)")
+    plt.ylabel("Block Size (Byte)")
+    plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.07),
+               ncol=3,fancybox=True, shadow=True,prop={'size':10})
+    plt.savefig('compTPPlt.png',dpi=300)
