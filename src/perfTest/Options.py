f6f2ec5db47a7acb3fd5fb4e926b145a2c4570e6
Georg Schönberger <gschoenberger@thomas-krenn.com>
2014-09-04 14:39:03 +0200

    Added toXml and fromXml

--- src/perfTest/Options.py
+++ src/perfTest/Options.py
@@ -4,6 +4,10 @@ Created on Aug 26, 2014
 @author: gschoenb
 '''
 
+import logging
+import json
+from lxml import etree
+
 class Options(object):
     '''
     A class holding user defined options on command line.
@@ -29,3 +33,31 @@ class Options(object):
     def setNj(self,nj): self.__numJobs = nj
     def setIod(self,iod): self.__ioDepth = iod
     def setXargs(self,xargs): self.__xargs = xargs
+    
+    def appendXml(self,r):
+        '''
+        Append the information about options to a XML node. 
+        @param root The xml root tag to append the new elements to
+        ''' 
+        data = json.dumps(list(self.__numJobs))
+        e = etree.SubElement(r,'numjobs')
+        e.text = data
+        
+        data = json.dumps(list(self.__ioDepth))
+        e = etree.SubElement(r,'iodepth')
+        e.text = data
+        
+        data = json.dumps(list(self.__xargs))
+        e = etree.SubElement(r,'xargs')
+        e.text = data
+
+    def fromXml(self,root):
+        '''
+        Loads the information about options from XML.
+        @param root The given element containing the information about
+        the object to be initialized.
+        '''
+        self.__numJobs = json.loads(root.findtext('numjobs'))
+        self.__ioDepth = json.loads(root.findtext('iodepth'))
+        self.__xargs = json.loads(root.findtext('xargs'))
+        logging.info("# Loading options from xml")
