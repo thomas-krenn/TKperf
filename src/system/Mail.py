b4e959d40789a52da4c8aaa95837ca464ca93619
Georg Schönberger <gschoenberger@thomas-krenn.com>
2014-09-26 12:51:52 +0200

    Send a PDF attachement via email

--- src/system/Mail.py
+++ src/system/Mail.py
@@ -6,26 +6,39 @@ Created on Sep 26, 2014
 
 import smtplib
 from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
 from email.mime.text import MIMEText
+from os import path
 
 class Mail(object):
     '''
     Sending emails via local smtp.
     '''
 
-
     def __init__(self, subj, sender, rcpt):
         '''
         Constructor
         '''
+        self.__sender = sender
+        self.__rcpt = rcpt
         self.__msg = MIMEMultipart()
         self.__msg['Subject'] = subj
         self.__msg['From'] = sender
-        self.__msgmsg['To'] = rcpt
+        self.__msg['To'] = rcpt
+        self.__smtp = smtplib.SMTP('mail.thomas-krenn.com')
 
     def addMsg(self, msg):
         msgText = MIMEText(msg, 'plain')
         self.__msg.attach(msgText)
 
-    def addAttachment(self,att):
-        msg.attach(MIMEText(file(att).read())
+    def addPDFAttachment(self,filename):
+        fp=open(filename,'rb')
+        att = MIMEApplication(fp.read(),_subtype="pdf")
+        fp.close()
+        name = path.basename(path.normpath(filename))
+        att.add_header('Content-Disposition','attachment',filename=name)
+        self.__msg.attach(att)
+
+    def send(self):
+        self.__smtp.sendmail(self.__sender, self.__rcpt, self.__msg.as_string())
+        self.__smtp.quit()
