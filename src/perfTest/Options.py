c5f52a52e1353163c4c2efd36bff1cbeb501e22e
Georg Schönberger <gschoenberger@thomas-krenn.com>
2015-09-09 08:32:15 +0200

    Add runtime attribute for options class
    Via the tkperf command line option 'runtime' one can now set
    the runtime attribute in an options object. If this is done the
    default runtime of 60 seconds is changed.

--- src/perfTest/Options.py
+++ src/perfTest/Options.py
@@ -13,7 +13,7 @@ class Options(object):
     A class holding user defined options on command line.
     '''
 
-    def __init__(self, nj=1, iod=1, xargs=None):
+    def __init__(self, nj=1, iod=1, runtime=60, xargs=None):
         '''
         Constructor
         @param nj Number of jobs
@@ -24,14 +24,18 @@ class Options(object):
         self.__nj = nj
         ## Number of iodepth for fio.
         self.__iod = iod
+        ## Runtime of one test round for fio.
+        self.__runtime = runtime
         ## Further single arguments as list for fio.
         self.__xargs = xargs
 
     def getNj(self): return self.__nj
     def getIod(self): return self.__iod
+    def getRuntime(self): return self.__runtime
     def getXargs(self): return self.__xargs
     def setNj(self,nj): self.__nj = nj
     def setIod(self,iod): self.__iod = iod
+    def setRuntime(self,rt): self.__runtime = rt
     def setXargs(self,xargs): self.__xargs = xargs
     
     def appendXml(self,r):
@@ -45,6 +49,10 @@ class Options(object):
         
         data = json.dumps(self.__iod)
         e = etree.SubElement(r,'iodepth')
+        e.text = data
+        
+        data = json.dumps(self.__runtime)
+        e = etree.SubElement(r,'runtime')
         e.text = data
         
         if self.__xargs != None:
@@ -62,6 +70,8 @@ class Options(object):
             self.__nj = json.loads(root.findtext('numjobs'))
         if root.findtext('iodepth'):
             self.__iod = json.loads(root.findtext('iodepth'))
+        if root.findtext('runtime'):
+            self.__runtime = json.loads(root.findtext('runtime'))
         if root.findtext('xargs'):
                 self.__xargs = json.loads(root.findtext('xargs'))
         logging.info("# Loading options from xml")
