a98b00e74fd4e668e2ae5a8164c7329ece1221c3
g.schoenberger <g.schoenberger@xortex.com>
2018-09-26 06:56:02 +0200

    Update setup.py with new github URL and author email

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from distutils.core import setup
 
 setup(name='TKperf',
-	version='2.1',
+	version='2.2',
 	description='TKperf - Thomas Krenn IO Performance Tests',
 	author='Georg Schoenberger',
-	author_email='gschoenberger@thomas-krenn.com',
-	url='https://github.com/thomas-krenn/TKperf_v1.git',
+	author_email='g.schoenberger@xortex.com',
+	url='https://github.com/thomas-krenn/TKperf.git',
 	package_dir = {'': 'src'},
 	packages = ['fio', 'perfTest','plots','reports','system'],
 	package_data  = {'reports':['pics/TKperf_logo.png']},
